1f84b049730ccad72922b510cdcc4c4f3217432b
Arda Ersoy <ardaaersoy@gmail.com>
2026-01-16 22:43:11 +0300

    Fix table view cell

--- Sources/AEComponentsKit/TableView/TableViewCell.swift
+++ Sources/AEComponentsKit/TableView/TableViewCell.swift
@@ -11,23 +11,36 @@ import Combine
 open class TableViewCell: UITableViewCell {
 
     public var cancellables = Set<AnyCancellable>()
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubviews()
+        setupConstraints()
+        addObservables()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     open override func awakeFromNib() {
         super.awakeFromNib()
         setupSubviews()
         setupConstraints()
         addObservables()
     }
-
-    open func setupSubviews() {}
     
-    open func setupConstraints() {}
-
-    open func addObservables() {}
-
     open override func prepareForReuse() {
         super.prepareForReuse()
         cancellables = []
         addObservables()
     }
+    
+    open func setupSubviews() {
+        selectionStyle = .none
+    }
+    
+    open func setupConstraints() {}
+    
+    open func addObservables() {}
 }
